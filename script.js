1f3c2e0a8f1bbc13fdb9c4d1e20d4a0f329e69d6
Mitsuri / <kcireh.i@gmail.com>
2026-09-23 17:00:50 -0300

    bug fixed 2

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 const tag_resultado = document.getElementById('tag_resultado')
 const tag_apostas = document.getElementById('tag_apostas')
 const tag_render = document.getElementById('tag_render')
+const tag_range1 = document.getElementById('tag_range1')
+const tag_range2 = document.getElementById('tag_range2')
 
 function numerosAleatorios(min, max, quantidade) {
     const numeros = new Set();
-
     while (numeros.size < quantidade) {
         numeros.add(Math.floor(Math.random() * max) + min);
     }
-
     return [...numeros].sort((a, b) => a - b);
 }
 
@@ -22,6 +22,9 @@ function quantidade_acertos_linha(res, lin) {
 }
 
 function joinspan(result, lin) {
+
+
+
     result = result.split(' ')
     lin = lin.split(' ')
 
@@ -36,64 +39,169 @@ function joinspan(result, lin) {
     ).join('')
 }
 
-function random6(min = 1, max = 60, lines = 4) {
-    // const min = 1
-    // const max = 60
-    const text = []
-    for (let j = 0; j < lines; j++) {
-        const lista = []
-        numerosAleatorios(min, max, 6).map(e => lista.push(e))
-        lista.map(e => text.push(e.toString().padStart(2, "0")))
-        if (j < lines - 1) text.push(";");
+const informationDraw = {
+    min: 1,
+    max: 60,
+    lines: 4,
+    quantity: 6
+}
+
+
+
+function generateNumbersDraw(obj) {
+
+    // generateNumbersDraw(informationDraw)
+
+    if (obj.min == undefined) obj.min = 1;
+    if (obj.max == undefined) obj.max = 60;
+    if (obj.lines == undefined) obj.lines = 1;
+    if (obj.quantity == undefined) obj.quantity = 6;
+
+    const t1 = []
+    for (let j = 0; j < obj.lines; j++) {
+
+        numerosAleatorios(obj.min, obj.max, obj.quantity).map(e => t1.push(e.toString().padStart(2, "0")))
+        // numerosAleatorios(obj.min, obj.max, obj.quantity).map(e => t1.push(e))
+        // t1.push("\n");
+        t1.push("\n");
     }
 
-    return text.join(" ").replace(/ ; /g, "\n")
+    return t1.slice(0, -1).join(" ").replace(/\n /g, "\n")
+
+
+    const text = []
+
+    for (let j = 0; j < obj.lines; j++) {
+        const lista = []
+        numerosAleatorios(obj.min, obj.max, obj.quantity).map(e => lista.push(e))
+        lista.map(e => text.push(e.toString().padStart(2, "0")))
+
+        if (j < obj.quantity) text.push(";");
+    }
+
+    // return text.slice(0, -1).join(" ").replace(/ ; /g, "\n")
+    return text.slice(0, -1).join(" ").replace(/ ; /g, "\n")
 
 }
 
-let resultado = random6(min = 1, max = 60, lines = 1)
-let apostas = random6()
+
+// function random6(min = 1, max = 60, lines = 4) {
+//     // const min = 1
+//     // const max = 60
+//     const text = []
+//     for (let j = 0; j < lines; j++) {
+//         const lista = []
+//         numerosAleatorios(min, max, 6).map(e => lista.push(e))
+//         lista.map(e => text.push(e.toString().padStart(2, "0")))
+//         if (j < lines - 1) text.push(";");
+//     }
+
+//     return text.join(" ").replace(/ ; /g, "\n")
+
+// }
+
+// let resultado = random6(min = 1, max = 60, lines = 1)
+// let apostas = random6()
+let resultado = generateNumbersDraw({ lines: 1 })
+let apostas = generateNumbersDraw(informationDraw)
+
 tag_resultado.value = resultado
 tag_apostas.value = apostas
 
-function randomize() {
-    let resultado = random6(min = 1, max = 60, lines = 1)
-    apostas = random6()
-    tag_resultado.value = resultado
-    tag_apostas.value = apostas
-    tag_render.innerHTML = render()
-}
+// function randomize() {
+//     let resultado = random6(min = 1, max = 60, lines = 1)
+//     apostas = random6()
+//     tag_resultado.value = resultado
+//     tag_apostas.value = apostas
+//     tag_render.innerHTML = render()
+// }
 
-function randomize2(l) {
-    resultado = random6(min = 1, max = 60, lines = 1)
-    apostas = random6(min = 1, max = 60, lines = l)
+// function randomize2(l) {
+//     resultado = random6(min = 1, max = 60, lines = 1)
+//     apostas = random6(min = 1, max = 60, lines = l)
+//     tag_resultado.value = resultado
+//     tag_apostas.value = apostas
+//     tag_render.innerHTML = render()
+// }
+
+function randomize3(obj) {
+
+    resultado = generateNumbersDraw({ quantity: obj.quantity })
+    apostas = generateNumbersDraw(obj)
+
     tag_resultado.value = resultado
     tag_apostas.value = apostas
-    tag_render.innerHTML = render()
+
+    render()
 }
 
 function render() {
-    let t1 = apostas
-    t1[t1.length - 1] = "x"
-
-    t1 = t1.split("\n").map(e => {
-        let txt = e.split(" ").join(" ")
-        return joinspan(resultado, txt) + "<span class='result'>" + quantidade_acertos_linha(resultado, txt) + "</span>"
-    }).join("<br>")
-    return t1
+    tag_render.innerHTML = ''
+    const apostar_split = apostas.trim().split('\n')
+    const resultado_arr = resultado.trim().split(' ').map(e => Number(e))
+    for (let i = 0; i < apostar_split.length; i++) {
+        const aposta_individual = apostar_split[i].trim().split(' ').map(e => Number(e))
+        tag_render.appendChild(Tag.check(resultado_arr, aposta_individual))
+    }
 }
 
-tag_render.innerHTML = render()
+class Tag {
+    static check(draw, bet) {
+        // draw = [12, 32, 21, 22, 39, 58]
+        // bet = [12, 32, 20, 20, 39, 58] 
+
+        const div = document.createElement('div')
+        let sum = 0
+
+        bet.map(e => draw.indexOf(e)).map((e, i) => {
+            const span = document.createElement('span')
+            // span.className = (e == -1) ? 'desactive' : 'active'
+
+            if (e == -1) {
+                span.className = 'desactive'
+            } else {
+                sum++
+                span.className = 'active'
+            }
+
+            // span.innerHTML = bet[i].toString().padStart(2, "0")
+            span.innerHTML = bet[i].toString().padStart(2, "0")
+            div.appendChild(span)
+        })
+
+        const span2 = document.createElement('span')
+        span2.classList.add('result')
+        span2.innerHTML = sum
+        div.appendChild(span2)
+
+        return div
+    }
+}
 
 tag_apostas.value = apostas
+
+render()
 
 tag_apostas.onkeyup = function () {
     tag_render.innerHTML = ""
     apostas = tag_apostas.value
-    tag_render.innerHTML = render()
+    render()
 }
 
 tag_resultado.onkeyup = function () {
     resultado = this.value
-    tag_render.innerHTML = render()
+    render()
 }
+
+
+tag_range1.addEventListener('mousemove', e => {
+    informationDraw.lines = tag_range1.value
+    document.querySelectorAll('.val')[0].innerHTML = informationDraw.lines
+    randomize3({ lines: informationDraw.lines, quantity: informationDraw.quantity })
+})
+
+tag_range2.addEventListener('mousemove', e => {
+    informationDraw.quantity = tag_range2.value
+    document.querySelectorAll('.val')[1].innerHTML = informationDraw.quantity
+    randomize3({ lines: informationDraw.lines, quantity: informationDraw.quantity })
+})
